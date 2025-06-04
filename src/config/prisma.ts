0b459e1093a290d959d598fe13c7c54c2535f4a1
hseun <hseun0732@dms.hs.kr>
2025-06-04 20:08:42 +0900

    chore :: 문구 추가

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient, ApplyType, DuringType, RequestLoanState, LoanState } from '@prisma/client';
 
 const prisma = new PrismaClient();
-console.log('Prisma has initted');
+console.log('Prisma has been initted');
 
 export { prisma, ApplyType, DuringType, RequestLoanState, LoanState };
