0e56fd9eb48f3d206fd4e011b154ab1dc9750ccf
hseun <hseun0732@dms.hs.kr>
2025-06-05 11:29:28 +0900

    feature :: OpenCode

--- src/controller/open.ts
+++ src/controller/open.ts
@@ -1,4 +1,4 @@
-import express, { Response } from 'express';
+import express, { Request, Response } from 'express';
 import { getApiLimit } from '../middleware/limit';
 import { AuthenticatedRequest } from '../types';
 import open from '../service/open';
@@ -6,6 +6,9 @@ import { verifyJWT } from '../middleware/jwt';
 
 const app = express.Router();
 
+app.get('/', getApiLimit, (req: Request, res: Response) => {
+  open.openCode(req, res);
+});
 app.get('/identification', getApiLimit, verifyJWT, (req: AuthenticatedRequest, res: Response) => {
   open.identificationUrl(req, res);
 });
---
+++ src/service/open/openCode.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { BasicResponse, REDIS_KEY } from '../../types';
+import redis from '../../config/redis';
+import { userToken } from './token';
+import { userInfo } from './userInfo';
+
+export const openCode = async (req: Request, res: Response<BasicResponse>) => {
+  try {
+    const { code, state } = req.query;
+    if (!code || !state) {
+      return res.status(400).json({
+        message: '잘못된 요청'
+      });
+    }
+
+    const codeStr = String(code);
+
+    const userIdStr = await redis.get(`${REDIS_KEY.OPEN_CODE_STATE} ${state}`);
+    if (!userIdStr) {
+      return res.status(404).json({
+        message: '발급되지 않은 state'
+      });
+    }
+
+    await userToken(codeStr, userIdStr);
+    await userInfo(codeStr, userIdStr);
+
+    return res.status(200).json({
+      message: '본인 인증 완료'
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      message: '서버 오류 발생'
+    });
+  }
+};
