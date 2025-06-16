3d91f386920c9daff10b50fbb4328ff21cb77e9f
hseun <hseun0732@dms.hs.kr>
2025-06-16 23:42:08 +0900

    update :: credit

--- src/utils/credit.ts
+++ src/utils/credit.ts
@@ -1,23 +1,21 @@
 import { prisma } from '../config/prisma';
 
-export const incrementCredit = async (userId: bigint, score: number) => {
-  try {
-    await prisma.user.update({
-      where: { id: userId },
-      data: { creditScore: { increment: score } }
-    });
-  } catch (err) {
-    throw err;
+type Direction = 'increment' | 'decrement';
+
+export const credit = async (userId: bigint, score: number, dir: Direction) => {
+  if (score <= 0) {
+    throw new Error('점수는 반드시 자연수');
   }
+  return prisma.user.update({
+    where: { id: userId },
+    data: { creditScore: { [dir]: score } }
+  });
+};
+
+export const incrementCredit = async (userId: bigint, score: number) => {
+  credit(userId, score, 'increment');
 };
 
 export const decrementCredit = async (userId: bigint, score: number) => {
-  try {
-    await prisma.user.update({
-      where: { id: userId },
-      data: { creditScore: { decrement: score } }
-    });
-  } catch (err) {
-    throw err;
-  }
+  credit(userId, score, 'decrement');
 };
